ab9ab5718617780a1fedd91e120e11afe8fb2154
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-05-03 11:08:03 -0500

    pg communicating with postico

--- server/routes/shoe.route.js
+++ server/routes/shoe.route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const pg = require('pg');
 const shoes = [
     {
         name: 'Red Wing',
@@ -19,17 +19,50 @@ const shoes = [
     }
 ];
 
+
+// ---- Move this connection to a module --- //
+
+// remember that the capital startchar means this is a constructor
+// this is a pool of connections options (to the database)
+// mongoose defined these things for us. PG allows/forces us to do it
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'shoe_store', // name of the datbase
+    host: 'localhost', // where is your database?
+    port: 5432, // the port here is the one that the database is running on.
+    // Postgress defaults to 5432
+
+    max: 10, // how many possible simultaneous connections (i.e. queries) 
+    // heroku allows 10 connections for its cost-free service
+
+    // this is the timeout time
+    idleTieoutMillis: 30000
+});
+
+// Set up success/error messages
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+});
+pool.on('error', (error) => {
+    console.log('error with Postgres pool', error);
+});
+
+// ^^ Move this to a connection module ^^ //
+
 router.post('/', (req, res) => {
-    console.log('bark');
-    const shoeToAdd = req.body;
-    console.log(shoeToAdd);
-    shoes.push(shoeToAdd);
-    console.log(shoes);
-    res.send(shoes);
-})
+    const newShoe = req.body;
+    pool.query(`INSERT INTO "shoes" ("name", "cost") 
+                VALUES ($1, $2); `, [newShoe.name, newShoe.cost])
+        .then((results) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('problem with post to database', error);
+            res.sendStatus(500);
+        });
+});
 
 router.get('/', (req, res) => {
-    console.log('meow');
     res.send(shoes);
 });
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,9 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const shoeRouter = require('./routes/shoe.route');
 
-// // mongoose connection
-// require('./modules/database-connection');
-
-
-// ('Adidas Superstar', 192),
-// ('Jordan XII', 178),
-// ('Converse Chuck Taylor low', 32),
-// ('Nike Roshe Run', 127),
-// ('Nike Huarache', 148); 
 
 // use body-parser
 app.use(bodyParser.json());
